c170be3cbd6c499ba266fa95fe49ea707352dcdb
Kyle Altendorf <sda@fstab.net>
2020-12-15 22:08:39 -0500

    add 5.15.2

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ def pad_version(v, segment_count=3):
 
 # TODO: really doesn't seem quite proper here and probably should come
 #       in some other way?
-pyqt_version = pad_version(os.environ.setdefault('PYQT_VERSION', '5.15.1'))
-qt_version = pad_version(os.environ.setdefault('QT_VERSION', '5.15.1'))
+pyqt_version = pad_version(os.environ.setdefault('PYQT_VERSION', '5.15.2'))
+qt_version = pad_version(os.environ.setdefault('QT_VERSION', '5.15.2'))
 qt_major_version = qt_version.partition('.')[0]
 
 pyqt5_plugins_wrapper_version = versioneer.get_versions()['version']
