bf7d69e40fae7280ab05c34ac3da169ac3654573
Kyle Altendorf <sda@fstab.net>
2021-05-21 16:45:14 -0400

    one less 5

--- src/pyqt_plugins/__init__.py
+++ src/pyqt_plugins/__init__.py
@@ -62,7 +62,7 @@ def create_environment(reference=None):
     environment = qt_tools.create_environment(reference=reference)
 
     if sys.platform in {'linux', 'darwin'}:
-        environment.update(pyqt5_plugins.utilities.add_to_env_var_path_list(
+        environment.update(pyqt_plugins.utilities.add_to_env_var_path_list(
             env=environment,
             name='LD_LIBRARY_PATH',
             before=[],
